49c40ca9423724e0da1ca307cfe33621f6a9a475
Steven Wilson <steven@dominion79.co.uk>
2019-11-12 10:55:07 +0000

    Configure gov design system
    - Update express tpl engine to use nunjucks
    - Update tmplates to use html
    - Configure express sass middleware
    - Add quick start page

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const sassMiddleware = require('node-sass-middleware')
+const nunjucks = require('nunjucks')
+const helmet = require('helmet')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index')
 
-var app = express();
+const app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+nunjucks.configure(
+  [
+    'views',
+    'node_modules/govuk-frontend/',
+    'node_modules/govuk-frontend/components/'
+  ], {
+    autoescape: true,
+    express: app
+  })
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'html')
+app.use(logger('dev'))
+app.use(helmet())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  debug: false,
+  outputStyle: 'compressed',
+  indentedSyntax: true,
+  sourceMap: true
+}))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/assets', express.static(path.join(__dirname, '/node_modules/govuk-frontend/assets')))
+app.use('/govuk-frontend', express.static(path.join(__dirname, '/node_modules/govuk-frontend')))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+module.exports = app
