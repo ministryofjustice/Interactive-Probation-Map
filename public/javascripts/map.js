9e07df67caa5fc1b19b6bb7ebe29710bb28abfc8
Steven Wilson <steven@dominion79.co.uk>
2019-11-19 19:06:02 +0000

    Load geojson into map
    - Add cutom styles for geojson layer
    - Add vector layer for geojson data

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,12 +1,48 @@
+// var json = './data/Police_Force_Areas_December_2016_Full_Extent_Boundaries_in_England_and_Wales.geojson'
+var json = './data/sample.geojson'
+
+var style = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: 'rgba(255, 255, 255, 0.6)'
+  }),
+  stroke: new ol.style.Stroke({
+    color: '#319FD3',
+    width: 1
+  }),
+  text: new ol.style.Text({
+    font: '12px Calibri,sans-serif',
+    fill: new ol.style.Fill({
+      color: '#000'
+    }),
+    stroke: new ol.style.Stroke({
+      color: '#fff',
+      width: 3
+    })
+  })
+})
+
+var vectorLayer = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    url: json,
+    format: new ol.format.GeoJSON()
+  }),
+  style: function (feature) {
+    style.getText().setText(feature.get('name'))
+    return style
+  }
+})
+
 var map = new ol.Map({
-  layers: [
+  layers:
+  [
     new ol.layer.Tile({
       source: new ol.source.OSM()
-    })
+    }),
+    vectorLayer
   ],
   target: 'map',
   view: new ol.View({
-    center: [0, 0],
+    center: [102.0, 0.5],
     zoom: 2
   })
 })
