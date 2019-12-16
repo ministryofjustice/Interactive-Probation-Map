f043d32017cb614c6464c118814ae25cd7b5364a
Steven Wilson <steve.wilson@digital.justice.gov.uk>
2019-12-16 15:34:14 +0000

    Feature/add web map service layer (#1)
    * Fix gitignore path for built css assets
    
    * Add sample geojson data
    
    * Load geojson into map
    
    - Add cutom styles for geojson layer
    - Add vector layer for geojson data
    
    * Remove styles.css and map from version control
    
    * Add basic openlayers mapp with WMS tile

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,12 +1,28 @@
+var wmsLayer = new ol.layer.Tile({
+  source: new ol.source.TileWMS({
+    url: 'https://ons-inspire.esriuk.com/arcgis/services/Administrative_Boundaries/Counties_April_2019_EN_BFC/MapServer/WMSServer?',
+    params: {
+      LAYERS: '0',
+      TILED: true
+    },
+    serverType: 'geoserver'
+    // Countries have transparency, so do not fade tiles:
+  })
+})
+
+var centerOnEngland = ol.proj.transform([-3.4359729, 55.3780518], 'EPSG:4326', 'EPSG:3857')
+
 var map = new ol.Map({
-  layers: [
+  layers:
+  [
     new ol.layer.Tile({
       source: new ol.source.OSM()
-    })
+    }),
+    wmsLayer
   ],
   target: 'map',
   view: new ol.View({
-    center: [0, 0],
-    zoom: 2
+    center: centerOnEngland,
+    zoom: 5
   })
 })
